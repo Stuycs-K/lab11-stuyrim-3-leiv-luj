37b7e60707229170170c844365d08c63fd4e362e
Joy Lu <joylu1308@gmail.com>
2025-01-21 01:05:45 -0500

    Wrote butcher class

--- Butcher.java
+++ Butcher.java
@@ -1,26 +1,20 @@
 public class Butcher extends Adventurer{
   int meatScraps, meatScrapsMax;
-  String preferredLanguage;
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
-  public Butcher(String name, int hp, String language){
+  public Butcher(String name, int hp){
     super(name,hp);
     meatScrapsMax = 12;
     meatScraps = meatScrapsMax/2;
-    preferredLanguage = language;
-  }
-
-  public Butcher(String name, int hp){
-    this(name,hp,"c++");
   }
 
   public Butcher(String name){
-    this(name,24);
+    this(name, 25);
   }
 
   public Butcher(){
-    this("Carmack");
+    this("Butcher");
   }
 
   /*The next 8 methods are all required because they are abstract:*/
@@ -45,34 +39,36 @@ public class Butcher extends Adventurer{
     int damage = (int)(Math.random()*3)+4;
     other.applyDamage(damage);
     restoreSpecial(2);
-    return this + " sliced "+ other + " with a butcher knife and dealt "+ damage +
-    " points of damage. Replenished 2 meat scraps.";
+    return this + " sliced "+ other + " with a butcher knife and dealt " + damage + " points of damage. Replenished 2 meat scraps.";
   }
 
-  /*Deal 9-12 damage to opponent if meatScraps is high enough (6).
-  *Reduces meatScraps by 5. Does 3 damage to self.
+  /*Deal 9-12 damage to opponent if meatScraps is high enough (8).
+  *Reduces meatScraps by 6. Does 3 damage to self.
   */
   public String specialAttack(Adventurer other){
-    if(getSpecial() >= 6){
-      setSpecial(getSpecial()-5);
+    if(getSpecial() >= 8){
+      setSpecial(getSpecial()-6);
       int damage = (int)(Math.random()*4)+9;
       other.applyDamage(damage);
-      return this + " used their "+preferredLanguage+
-      " skills to hack the matrix. "+
-      " This glitched out "+other+" dealing "+ damage +" points of damage.";
+      this.applyDamage(3);
+      return this + " attacked with their meat cleaver, dealing " + damage + " points of damage to " + other + ", but took 3 points of recoil damage.";
     }else{
-      return "Not enough meatScraps to use the ultimate attack. Instead "+attack(other);
+      return "Not enough meatScraps to use the meat cleaver. Instead, "+attack(other);
     }
-
   }
-  /*makes team do +3 damage*/
-  public String support(Adventurer other){
-    return "Served up a meat feast for " + other + ". " + other + "'s attacks now do +3 more damage.";
+  /*heals team +3 hp*/
+  public String support(Adventurer other) {
+    setSpecial(getSpecial()-2);
+    int hp = 3;
+    other.setHP(other.getHP() + hp);
+    return this + " used 2 meat scraps and served a meat feast to " + other + ", restoring " + hp + " HP.";
   }
-  /*makes self do +3 damage*/
+  /*restores 4 hp and 2 meat scraps*/
   public String support(){
-    return "";
-    // this + " dines on a steak and "+restoreSpecial(6)+" "
-    // + getSpecialName()+ " and "+ hp +" HP";
+    int hp = 4;
+    setHP(getHP() + hp);
+    int scraps = 2;
+    setSpecial(getSpecial() + 2);
+    return this + " dined on a steak, restoring " + hp + " HP and " + scraps + " " + getSpecialName() + ".";
   }
 }
