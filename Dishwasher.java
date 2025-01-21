1a46c2dd5b935deff2218e2d9e055e979fb25fd7
Joy Lu <joylu1308@gmail.com>
2025-01-21 01:11:21 -0500

    Fixed dishwasher, made attacks work

--- Dishwasher.java
+++ Dishwasher.java
@@ -1,26 +1,20 @@
 public class Dishwasher extends Adventurer{
   int soap, soapMax;
-  String preferredLanguage;
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
-  public Dishwasher(String name, int hp, String language){
+  public Dishwasher(String name, int hp){
     super(name,hp);
     soapMax = 12;
     soap = soapMax/2;
-    preferredLanguage = language;
-  }
-
-  public Dishwasher(String name, int hp){
-    this(name,hp,"c++");
   }
 
   public Dishwasher(String name){
-    this(name,24);
+    this(name,25);
   }
 
   public Dishwasher(){
-    this("Carmack");
+    this("Dishwasher");
   }
 
   /*The next 8 methods are all required because they are abstract:*/
@@ -45,8 +39,7 @@ public class Dishwasher extends Adventurer{
     int damage = (int)(Math.random()*6)+2;
     other.applyDamage(damage);
     restoreSpecial(2);
-    return this + " threw dirty dishes at "+ other + " and dealt "+ damage +
-    " points of damage. " + this + "replenishes 2 soap.";
+    return this + " threw dirty dishes at "+ other + " and dealt "+ damage + " points of damage. " + this + "replenishes 2 soap.";
   }
 
   /*Deal 3-12 damage to opponent, only if soap is high enough.
@@ -57,24 +50,23 @@ public class Dishwasher extends Adventurer{
       setSpecial(getSpecial()-8);
       int damage = (int)(Math.random()*5+Math.random()*5)+3;
       other.applyDamage(damage);
-      return this + " used their "+preferredLanguage+
-      " skills to hack the matrix. "+
-      " This glitched out "+other+" dealing "+ damage +" points of damage.";
+      return this + " threw a bacteria infested soap sponge at "+ other +"." + " This dealt "+ damage +" points of damage to " + other + ".";
     }else{
-      return "Not enough soap to use the ultimate code. Instead "+attack(other);
+      return "Not enough soap to use the special attack. Instead, "+attack(other);
     }
 
   }
-  /* minus 3 damage taken by teammates */
+  /* +4 special of teammates */
   public String support(Adventurer other){
-    return "Cleans weapons of "+other+" and restores "
-    + other.restoreSpecial(5)+" "+other.getSpecialName();
+    setSpecial(getSpecial()-2);
+    return this + " used 2 soap to clean the knives of " + other + " and restored "
+    + other.restoreSpecial(4)+" "+other.getSpecialName();
   }
-  /* minus 3 damage taken by self */
+  /* heals 2 damage taken by self */
   public String support(){
-    int hp = 1;
+    setSpecial(getSpecial()-2);
+    int hp = 2;
     setHP(getHP()+hp);
-    return this+" cleans knives "+restoreSpecial(6)+" "
-    + getSpecialName()+ " and "+hp+" HP";
+    return this+" used 2 soap to clean knives and restored "+ hp+" HP.";
   }
 }
